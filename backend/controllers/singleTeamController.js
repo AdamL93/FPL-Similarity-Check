cc3c0bdd79c5003fac9dd4ac823e0ff57ad6788d
Adam Lynn <adam.lynn@live.com>
2024-07-04 17:52:32 +0100

    Adjusted backendlogic to readjson file.
    added parse data to retreive only by teamid
    
    formatted the data so it reads vertically.

--- backend/controllers/singleTeamController.js
+++ backend/controllers/singleTeamController.js
@@ -4,34 +4,25 @@ const fs = require('fs/promises');
 require('dotenv').config()
 
 
-
 const getGameweekData = async (request, response) => {
 
     const { teamId } = request.params //gets teamid from route properties
     try {
         const data = await fs.readFile('../Creating_database/json_data/personal_league_gameweek_data.json', { encoding: 'utf8' });
+        const jsonData = JSON.parse(data);
+
+        const team_data = JSON.stringify(jsonData.filter(item => item.team_id === Number(teamId)), null, 2);
+
+        console.log(typeof team_data)
         console.log(`successfully read data for ${teamId}`)
-        return response.status(200).json(data);
+
+        // Return formatted data as JSON response
+        console.log("data received")
+        return response.status(200).json(team_data);
+
     } catch (err) {
         return response.status(404).json({error: 'unable to retreive data'});
     }
 };
 
-
 module.exports = {getGameweekData};
-
-/*const getGameweekData = async (request, response) => {
-    const { teamId } = request.params //gets teamid from route properties
-
-    const db = mongoose.connection.db;
-
-    const gameweekData = await GameWeekDataModel.findOne({})
-
-    if (!gameweekData) {
-        return response.status(404).json({message: 'Invalid Team Id'})
-    }
-    console.log(gameweekData);
-    response.json(gameweekData)
-}
-*/
-
