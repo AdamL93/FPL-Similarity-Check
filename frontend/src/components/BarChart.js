9c577fb8e3adcbe5d7a0b2409eb351aa58505352
ALynnStrath <adam.lynn.2023@uni.strath.ac.uk>
2024-07-13 14:23:32 +0100

    Adjusted formatting of graphs.
    Added labels and positioned.

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -5,12 +5,24 @@ import { XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar, Rectangle}
 function BarChartComponent({ data }) {
 
     return (
-        <BarChart width={600} height={400} data={data} margin={{top: 5,right: 30,left: 20,bottom: 5,}}>
-            <XAxis dataKey="gameweek" />
-            <YAxis domain={[0,100]}/>
+        <BarChart width={800} height={400} data={data} margin={{top: 5,right: 30,left: 20,bottom: 5,}}>
+            <XAxis 
+            dataKey="gameweek" 
+            interval={0} 
+            tick={{ fontSize: 12 }} 
+            label={{ value: 'Gameweek Number', 
+                    position: 'insideBottom', 
+                    offset: -5 }}
+            />
+            <YAxis 
+            domain={[0,100]} 
+            label={{ value: 'Similarity %', 
+                    angle: -90, 
+                    position: 'insideLeft', dy:40 }} 
+            />
             <CartesianGrid stroke="grey" strokeDasharray="2 2" />
             <Bar dataKey="similarity" name="Similarity %" fill="purple" activeBar={<Rectangle stroke="purple"/>} barSize ={8} barGap={1} />
-            <Legend />
+            <Legend verticalAlign="top" align="center" wrapperStyle={{ marginLeft: 40 }}/>
             <Tooltip />
         </BarChart>
 
--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,16 +1,29 @@
-import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip, Legend, BarChart, Bar, Rectangle} from 'recharts';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip, Legend} from 'recharts';
 
 
 
 function LineChartComponent({ data }) {
 
     return (
-        <LineChart width={600} height={400} data={data}>
-            <XAxis dataKey="gameweek" />
-            <YAxis domain={[0, 100]} />
+        <LineChart width={800} height={400} data={data}>
+            <XAxis 
+                dataKey="gameweek" 
+                interval={0} 
+                tick={{ fontSize: 12 }} 
+                label={{ value: 'Gameweek Number', 
+                        position: 'insideBottom', 
+                        offset: -5 }} 
+            />
+            <YAxis 
+            domain={[0, 100]} 
+            label={{ value: 'Similarity %', 
+                    angle: -90, 
+                    position: 'insideLeft', 
+                    dy:40 }} 
+            />
             <CartesianGrid stroke="grey" strokeDasharray="2 2" />
-            <Line type="linear" dataKey="similarity" name ="Similarity % by Gameweek" stroke="purple" strokeWidth={2} dot={false} />
-            <Legend />
+            <Line type="linear" dataKey="similarity" name ="Similarity %" stroke="purple" strokeWidth={2} dot={false} />
+            <Legend verticalAlign="top" align="center" wrapperStyle={{ marginLeft: 40 }}/>
             <Tooltip />
         </LineChart>
 
