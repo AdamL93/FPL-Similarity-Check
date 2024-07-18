dc60657bc424de0e981a6f65ef5d175e6048c007
ALynnStrath <adam.lynn.2023@uni.strath.ac.uk>
2024-07-18 19:50:34 +0100

    corrected average similarity number. made it dynamic.

--- frontend/src/components/CreatorResultsComponent.js
+++ frontend/src/components/CreatorResultsComponent.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-function CreatorResultsComponent({ title, description }) {
+function CreatorResultsComponent({ title, description, textColour}) {
+
+
+
   return (
     <Card style={{ width: '18rem',  marginTop: '40px'}}>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
+        <Card.Title style={{ color: 'purple' }}>{title}</Card.Title>
+        <Card.Text style={{ color: textColour }}>{description}</Card.Text>
       </Card.Body>
     </Card>
   );
