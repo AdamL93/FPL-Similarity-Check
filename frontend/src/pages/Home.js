bb5ee7811f05694a55f1a8b45a9ec97d545d7d34
Adam Lynn <adam.lynn@live.com>
2024-07-18 14:28:18 +0100

    Added check in home pagge to naviagte to correct page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,7 +10,9 @@ const Home = () => {
 
     const handleSearchSubmit = (teamId, teamId2) => {
         console.log(teamId, teamId2)
-        navigate('/ResultsPage', { state: { inputValue: teamId, inputValue2: teamId2 } });
+
+        let navigateRoute = (teamId2 === '') ? ('/CreatorResults') : ('/ResultsPage');
+        navigate(navigateRoute, { state: { inputValue: teamId, inputValue2: teamId2 } });
     };
     
     
