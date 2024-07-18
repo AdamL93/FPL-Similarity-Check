37f8c3d93eaa2d0c3743b785809ba5750b9c82c2
ALynnStrath <adam.lynn.2023@uni.strath.ac.uk>
2024-07-18 21:10:23 +0100

    Added filter to Search Bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,16 @@
 
 import SearchBar from '../components/SearchBar'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 
 
 const Home = () => {
+
+    const [filterState, setFilterState] = useState(false);
 
     const navigate = useNavigate()
 
@@ -16,11 +22,22 @@ const Home = () => {
         //add if else to only send a single teamId if temaId2 is ''
         navigate(navigateRoute, { state: { inputValue: teamId, inputValue2: teamId2 } });
     };
+
+    const toggleFilter = () => {
+        setFilterState(prevState => !prevState);
+      };
     
     
     return (
         <Container fluid="xs" className="home">
-                <SearchBar handleSearchSubmit={handleSearchSubmit}/>
+            <Row>
+                    <SearchBar 
+                        handleSearchSubmit={handleSearchSubmit}
+                        toggleFilter={toggleFilter}
+                        filterState={filterState}
+                    />
+            </Row>
+                
         </Container>
     );
 };
