8ca1ce2c6677364b40f77d6dcfc990cfa3ce4dba
ALynnStrath <adam.lynn.2023@uni.strath.ac.uk>
2025-05-21 16:35:21 +0100

    added function in services to retreive and return an array for teamNames

--- backend/services/fplService.js
+++ backend/services/fplService.js
@@ -24,4 +24,32 @@ const getTeamName = async (request, response) => {
 
 // similar to above but using promise.all() for multiple creator teams.
 // decide whether the promise.all() should be here or further back in the code.
+
+const getTeamNames = async (teamIds) => {
+
+    const teamNameUrls = [];
+    const teamNames = [];
+
+
+    //create urls from team ids array
+    for (let i = 0; i < teamIds.length; i++) {
+        teamNameUrls.push(`https://fantasy.premierleague.com/api/entry/${teamIds[i]}/`)
+    }
+
+    try {
+        // returns an array of json objects for each url
+        const responses = await Promise.all(teamNameUrls.map(url => 
+            fetch(url).then(response => response.json())
+        ));
+
+        responses.forEach((response, index) => {
+            // extract team name and add to array
+            teamNames.push(response.name)
+        })
+        return teamNames;
+        
+    } catch (err) {
+        console.log("Failed to fetch content creator team names", err)
+    }
+}
 module.exports = { getTeamName };
